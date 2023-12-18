b2b0a61570959b3b99df6c1caa7b69cbef5bef00
roka15 <rokaisback0105@gmail.com>
2023-12-18 21:11:17 +0900

    lib define 추가 및 수정

--- Project/RokaSTL_Lib/define.h
+++ Project/RokaSTL_Lib/define.h
@@ -3,4 +3,8 @@
 
 #define MemoryLeakCheck _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #define SINGLETON(type) private: type();~type();friend class rokaStl::Singleton<type>;
+#define MCreate(type) type::Create();
+#define MDestroy(type) type::Destroy();
 #define GetInstance(type) type::GetInstance();
+
+#define TYPETOINT(type) (UINT)(type)
