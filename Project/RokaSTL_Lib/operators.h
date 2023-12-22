5d637eabbc5fdd788c9d9061a48643c6be26aa14
roka15 <rokaisback0105@gmail.com>
2023-12-22 16:21:11 +0900

    stl lib include - memorypool, objectpool,operators 추가 operator - rokaproj에 있던것을 stl lib로 옮김.

--- Project/RokaSTL_Lib/operators.h
+++ Project/RokaSTL_Lib/operators.h
@@ -40,14 +40,57 @@ struct PCharGreater
 {
 	bool operator()(const char* data1, const char* data2)
 	{
-		return *data1 < *data2;
+		size_t len1 = strlen(data1);
+		size_t len2 = strlen(data2);
+		if (len1 != len2)
+		{
+			if (len1 < len2)
+				return true;
+			else if (len1 > len2)
+				return false;
+		}
+
+
+		size_t len = len1;
+		for (int i = 0; i < len; ++i)
+		{
+			if (data1[i] == data2[i])
+				continue;
+			else
+			{
+				return data1[i] > data2[i];
+			}
+		}
+
+		return false;
 	}
 };
 struct PCharLess
 {
 	bool operator()(const char* data1, const char* data2)
 	{
-		return *data1 > *data2;
+		size_t len1 = strlen(data1);
+		size_t len2 = strlen(data2);
+		if (len1 != len2)
+		{
+			if (len1 > len2)
+				return true;
+			else if (len1 < len2)
+				return false;
+		}
+
+		size_t len = len1;
+		for (int i = 0; i < len; ++i)
+		{
+			if (data1[i] == data2[i])
+				continue;
+			else
+			{
+				return data1[i] < data2[i];
+			}
+		}
+
+		return false;
 	}
 };
 
